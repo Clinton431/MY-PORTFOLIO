5b6750871413fa4ebdc1b4c3b50c6a9a61edaffb
Clinton431 <clintonnyakoe@gmail.com>
2024-10-15 08:18:11 +0300

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Clinton Nyakoe",
+  title: "Full Stack Engineer & Content Creator",
+  email: "clintonnyakoe@gmail.com",
+  gitHub: "clinton431",
+  instagram: "clinnyakoe",
+  linkedIn: "clintonnyakoe",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Clintonnyakoe",
+  youTube: "Clinton Nyakoe",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,15 +24,16 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a UI/UX student studying at  Murang'a  University Of Technology. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
+  "Front-End Web design",
+  "Back-End Web design",
+  "Bash Scripting",
   "Inclusive design",
   "Focus group testing",
   "Mobile user interfaces",
